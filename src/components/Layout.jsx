1ffc3c9e1106f84f4bfae8dc06ffdb94257e82d2
EGERHARDS <edga0327@stud.kea.dk>
2023-04-18 13:11:22 +0200

    doggi site

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import Anchor from "./Anchor";
+
+export default function Layout({ children, navData }) {
+  return (
+    <>
+      <nav>
+        {navData.map((obj) => {
+          return <Anchor href={"/dogs/" + obj.slug}>{obj.title}</Anchor>;
+        })}
+      </nav>
+      {children}
+      <footer>Footer</footer>
+    </>
+  );
+}
